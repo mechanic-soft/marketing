35711b8db5d367025ce0121357c2122d25657acf
zehongxie <666666>
2018-07-19 10:06:54 +0800

    客户管理-删除

---
+++ mkt-api/src/main/java/cn/com/geasy/marketing/api/customer/CustomerController.java
@@ -0,0 +1,43 @@
+package cn.com.geasy.marketing.api.customer;
+
+import cn.com.geasy.marketing.domain.dto.system.SysUserDto;
+import cn.com.geasy.marketing.mapstruct.system.SysUserMapstruct;
+import cn.com.geasy.marketing.service.customer.CustomerService;
+import com.gitee.mechanic.web.utils.ResponseUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+客户管理
+ */
+@Api(tags = "Login", description = "客户管理接口")
+@Slf4j
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerSrv;
+
+    @ApiOperation(value = "删除客户信息")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "ids", value = "客户ID", paramType = "body")
+    })
+    @DeleteMapping(path = "/customers", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ModelMap> getUser(@RequestBody List<Long> ids){
+        return ResponseUtils.result(customerSrv.deleteBatchIds(ids));
+    }
+
+
+
+
+}
---
+++ mkt-dao/src/main/java/cn/com/geasy/marketing/dao/customer/CustomerMapper.java
@@ -0,0 +1,12 @@
+package cn.com.geasy.marketing.dao.customer;
+
+import cn.com.geasy.marketing.domain.entity.customer.Customer;
+import com.gitee.mechanic.mybatis.base.SuperMapper;
+
+/**
+ * 客户管理Mapper
+ */
+public interface CustomerMapper extends SuperMapper<Customer> {
+
+}
+
