daafcda33d3e7e43e12998f814395ebafb540fb5
genchengpan <441354512@qq.com>
2018-07-20 18:55:51 +0800

    任务待办相关方法

---
+++ mkt-dao/src/main/java/cn/com/geasy/marketing/dao/task/TaskUserMapper.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ * Created on 2018/7/12 下午6:04
+ */
+package cn.com.geasy.marketing.dao.task;
+
+import cn.com.geasy.marketing.domain.dto.task.TaskDto;
+import cn.com.geasy.marketing.domain.entity.task.Task;
+import cn.com.geasy.marketing.domain.entity.task.TaskUser;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+import com.gitee.mechanic.mybatis.base.SuperMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 待办任务数据访问接口
+ *
+ * @author gencheng.pan
+ * @version 1.0.0
+ */
+public interface TaskUserMapper extends SuperMapper<TaskUser> {
+
+    List<TaskUser> findDailyTaskByUserId(Long userId);
+    //List<Task> findByUserIdPage(@Param("title")String title, @Param("userId") Long userId);
+
+//    List<ArticleHotDto> findByHotArticlePage();
+
+    //List<Article> findBySubscribePage(@Param("userId") Long userId);
+
+    //List<Article> findByLikePage(@Param("userId") Long userId);
+
+//    List<ArticleReadRecordDto> findRecentReadRecord(@Param("userId") Long userId);
+    /**
+     * 分页查询待办任务信息
+     * @param taskDto 查询条件
+     * @param pagination 分页信息
+     * @return List&lt;TaskDto&gt; 待办任务信息
+     */
+     Page<Task> selectAllPage(@Param("td") TaskDto taskDto, Pagination pagination);
+}
