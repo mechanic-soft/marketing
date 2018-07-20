a8ef2c9cdd53f81190ada7ac5eca1d2e4f055db1
Phil <q527011@gmail.com>
2018-07-20 14:10:33 +0800

    删了shiro相关代码

--- mkt-service/src/main/java/cn/com/geasy/marketing/service/customer/impl/CustomerServiceImpl.java
+++ mkt-service/src/main/java/cn/com/geasy/marketing/service/customer/impl/CustomerServiceImpl.java
@@ -9,9 +9,6 @@ import cn.com.geasy.marketing.service.customer.CustomerService;
 import cn.com.geasy.marketing.service.wechat.WxContactService;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.gitee.mechanic.mybatis.base.SuperServiceImpl;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.session.Session;
-import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -56,12 +53,12 @@ public class CustomerServiceImpl extends SuperServiceImpl<CustomerMapper, Custom
         return dbCustomerDto;
     }
 
-    private Session getCurrentUser(){
-        //shiro管理的session
-        Subject currentUser = SecurityUtils.getSubject();
-        Session session = currentUser.getSession();
-        session.getAttribute("user");
-        return SecurityUtils.getSubject().getSession();
-    }
+//    private Session getCurrentUser(){
+//        //shiro管理的session
+//        Subject currentUser = SecurityUtils.getSubject();
+//        Session session = currentUser.getSession();
+//        session.getAttribute("user");
+//        return SecurityUtils.getSubject().getSession();
+//    }
 }
 
