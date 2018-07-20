7de1fc9f2b66d12fa9f6e32cececcf4a80b878f4
genchengpan <441354512@qq.com>
2018-07-20 18:57:53 +0800

    任务待办相关方法

---
+++ mkt-domain/src/main/java/cn/com/geasy/marketing/domain/dto/task/TaskDto.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ * Created on 2018/7/12 下午5:06
+ */
+package cn.com.geasy.marketing.domain.dto.task;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.gitee.mechanic.mybatis.base.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 待办任务Dto
+ *
+ * @author gencheng.pan
+ * @version 1.0.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDto implements Serializable {
+    private static final long serialVersionUID = 5276473800609171477L;
+    /**
+     * 主键
+     */
+    private Long id;
+    /**
+     * 标题
+     */
+    private String title;
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 状态(0=删除,1=正常)
+     */
+    private Integer status;
+
+    /**
+     * 用户id
+     */
+    private List<Long> userId;
+/*    *//**
+     * 创建记录的用户id
+     *//*
+    private BigInteger createUserId;
+    *//**
+     * 创建记的时间
+     *//*
+    private LocalDateTime createTime;
+    *//**
+     * 记录最后一次更新的用户id
+     *//*
+    private BigInteger updateUserId;
+    *//**
+     * 更改记录的时间
+     *//*
+    private LocalDateTime updateTime;*/
+}
