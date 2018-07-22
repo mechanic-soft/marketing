d5b0ca26980cd68f80b6fc5880ce1106f7dd5aec
genchengpan <441354512@qq.com>
2018-07-22 17:36:37 +0800

    任务待办相关方法

---
+++ mkt-dao/src/main/java/cn/com/geasy/marketing/dao/task/RuleCustomerLabelMapper.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ * Created on 2018/7/12 下午6:04
+ */
+package cn.com.geasy.marketing.dao.task;
+
+
+import cn.com.geasy.marketing.domain.entity.task.RuleCustomerLabel;
+import com.gitee.mechanic.mybatis.base.SuperMapper;
+
+
+/**
+ * 规则客户标签关联数据访问接口
+ *
+ * @author gencheng.pan
+ * @version 1.0.0
+ */
+public interface RuleCustomerLabelMapper extends SuperMapper<RuleCustomerLabel> {
+
+    //List<Task> findByUserIdPage(@Param("title")String title, @Param("userId") Long userId);
+
+//    List<ArticleHotDto> findByHotArticlePage();
+
+    //List<Article> findBySubscribePage(@Param("userId") Long userId);
+
+    //List<Article> findByLikePage(@Param("userId") Long userId);
+
+//    List<ArticleReadRecordDto> findRecentReadRecord(@Param("userId") Long userId);
+    /**
+     * 分页查询待办任务信息
+     * @param taskDto 查询条件
+     * @param pagination 分页信息
+     * @return List&lt;TaskDto&gt; 待办任务信息
+     */
+     //Page<Task> selectAllPage(@Param("td") TaskDto taskDto, Pagination pagination);
+}
