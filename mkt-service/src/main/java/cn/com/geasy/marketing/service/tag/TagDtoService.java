5e39383c1c977602dac268266ab5799a6b423ea3
zehongxie <666666>
2018-07-23 15:46:37 +0800

    分页查询TagDto 列表

--- mkt-service/src/main/java/cn/com/geasy/marketing/service/tag/TagDtoService.java
+++ mkt-service/src/main/java/cn/com/geasy/marketing/service/tag/TagDtoService.java
@@ -6,6 +6,7 @@ package cn.com.geasy.marketing.service.tag;
 
 import cn.com.geasy.marketing.domain.dto.tag.TagDto;
 import cn.com.geasy.marketing.domain.entity.tag.Tag;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.gitee.mechanic.mybatis.base.SuperService;
 
 import java.util.List;
@@ -16,6 +17,18 @@ import java.util.List;
  * @version 1.0.0
  */
 public interface TagDtoService extends SuperService<TagDto> {
-
+   /**
+    * 根据客户id 查询TagDto 全部列表数据
+    * @param customerId
+    * @return
+    */
    public List<TagDto> selectTagDtoList(Long customerId);
+
+   /**
+    * 分页查询TagDto 列表
+    * @param pageNum
+    * @param tagDto
+    * @return
+    */
+   public Page<TagDto> selectDtoPage(int pageNum, TagDto tagDto);
 }
