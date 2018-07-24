2a176dd59f4c787558219ba2d597299d62b52e58
yml <874511058@qq.com>
2018-07-24 08:20:17 +0800

    完善常量注释

--- mkt-service/src/main/java/cn/com/geasy/marketing/contant/Const.java
+++ mkt-service/src/main/java/cn/com/geasy/marketing/contant/Const.java
@@ -4,16 +4,60 @@ package cn.com.geasy.marketing.contant;
  * Created by yml on 2018/7/19.
  */
 public class Const {
-
+    /**
+     * 成功字符串常量
+     */
     public static final String SUCCESS="成功";
+    /**
+     *失败字符串常量
+     */
     public static final String FAIL="失败";
+    /**
+     * 保存成功字符串常量
+     */
     public static final String SAVE_SUCCESS="保存成功";
+    /**
+     *保存失败字符串常量
+     */
     public static final String SAVE_FAIL="保存失败";
+    /**
+     *修改成功字符串常量
+     */
+    public static final String UPDATE_SUCCESS="修改成功";
+    /**
+     * 修改失败字符串常量
+     */
+    public static final String UPDATE_FAIL="修改失败";
+    /**
+     关联成功字符串常量
+     */
     public static final String RELE_SUCCESS="关联成功";
+    /**
+     *关联失败字符串常量
+     */
     public static final String RELE_FAIL="关联失败";
+    /**
+     *同步成功字符串常量
+     */
     public static final String SYNCHRONIZE_SUCCESS="同步成功";
+    /**
+     *同步失败字符串常量
+     */
     public static final String SYNCHRONIZE_FAIL="同步失败";
+    /**
+     *
+     */
     public static final Integer ONE = 1;
+    /**
+     *删除成功字符串常量
+     */
     public static final String DELETE_SUCCESS="删除成功";
+    /**
+     *删除失败字符串常量
+     */
     public static final String DELETE_FAIL="删除失败";
+    /**
+     *斜杠标识符常量
+     */
+    public static final String SPRIT = "/";
 }
