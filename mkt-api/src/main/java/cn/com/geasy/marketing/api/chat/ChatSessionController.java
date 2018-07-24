016155212a35304e83a6e89a89c77cfb21397109
genchengpan <441354512@qq.com>
2018-07-24 09:48:25 +0800

    聊天会话相关方法

---
+++ mkt-api/src/main/java/cn/com/geasy/marketing/api/chat/ChatSessionController.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ * Created on 2018/7/10 下午8:46
+ */
+package cn.com.geasy.marketing.api.chat;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 聊天会话Controller
+ *
+ * @author gencheng.pan
+ * @version 1.0.0
+ */
+@Api(tags = "chatSession", description = "聊天会话接口")
+@Slf4j
+@RestController
+@RequestMapping(path = "/v1")
+public class ChatSessionController {
+
+
+
+}
