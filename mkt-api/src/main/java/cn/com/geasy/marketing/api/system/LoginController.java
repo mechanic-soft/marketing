702ff8d0237e44957a6ea949a53bc374f06f14d8
Phil <q527011@gmail.com>
2018-07-25 18:10:07 +0800

    配置了logback日志。

--- mkt-api/src/main/java/cn/com/geasy/marketing/api/system/LoginController.java
+++ mkt-api/src/main/java/cn/com/geasy/marketing/api/system/LoginController.java
@@ -7,9 +7,9 @@ package cn.com.geasy.marketing.api.system;
 import com.gitee.mechanic.core.enums.HttpCode;
 import com.gitee.mechanic.web.utils.ResponseUtils;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +24,10 @@ import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -38,9 +41,9 @@ import java.io.IOException;
  * @version 1.0.0
  */
 @Api(tags = "Login", description = "登录接口")
-@Slf4j
 @RestController
 public class LoginController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final AuthenticationManager authenticationManager;
 
@@ -55,10 +58,6 @@ public class LoginController {
         this.authenticationManager = authenticationManager;
     }
 
-    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "user", value = "登录账户", required = true, dataType = "String", paramType = "form"),
-//            @ApiImplicitParam(name = "pass", value = "登录密码", required = true, dataType = "String", paramType = "form")
-    })
     @ApiOperation(value = "用户登录")
     @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<ModelMap> login(HttpServletRequest request,
@@ -73,57 +72,25 @@ public class LoginController {
             SecurityContextHolder.getContext().setAuthentication(authentication);
             HttpSession session = request.getSession();
             session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
-//            log.debug("用户[" + username + "]已成功登录。");
+            logger.debug("用户[" + username + "]已成功登录。");
             return ResponseUtils.result("用户[" + username + "]已成功登录。");
         } catch (AuthenticationException ex) {
             return ResponseUtils.result(ex.getMessage());
         }
     }
 
-    //    @ApiOperation(value = "用户登录状态")
-//    @GetMapping(value = "status")
-//    public ResponseEntity<ModelMap> isLogin() {
-//        Subject subject = SecurityUtils.getSubject();
-//        if (subject.isAuthenticated()) {
-//            return ResponseUtils.result("用户[" + subject.getPrincipal() + "]已登录。");
-//        } else {
-//            return ResponseUtils.result(HttpCode.UNAUTHORIZED);
-//        }
-//    }
-//
-//    @ApiOperation(value = "用户退出登录")
-//    @GetMapping(value = "logout")
-//    public ResponseEntity<ModelMap> logout() {
-//        Subject subject = SecurityUtils.getSubject();
-//        subject.logout();
-//        return ResponseUtils.result("用户已退出登录。");
-//    }
-//
     @ApiOperation(value = "用户未登录时跳转到的接口")
     @GetMapping("/unauthor")
     public ResponseEntity<ModelMap> unauthor() {
         return ResponseUtils.result(HttpCode.UNAUTHORIZED);
     }
 
-    //
-//    @ApiOperation(value = "用户没有权限时跳转到的接口")
-//    @GetMapping("forbidden")
-//    public ResponseEntity<ModelMap> forbidden() {
-//        return ResponseUtils.result(HttpCode.FORBIDDEN);
-//    }
-//
-//    @ApiOperation(value = "用户未登录或退出时时跳转到的接口")
-//    @GetMapping("/")
-//    public ResponseEntity<ModelMap> index(HttpServletRequest request) {
-//        return ResponseUtils.result(HttpCode.UNAUTHORIZED);
-//    }
     @ApiOperation(value = "退出登录接口")
     @GetMapping("/logout")
     public ResponseEntity<ModelMap> logout(HttpServletRequest request, HttpServletResponse response) {
 
         HttpSession session = request.getSession(false);
         if (session != null) {
-//            log.debug("Invalidating session: " + session.getId());
             session.invalidate();
         }
 
--- mkt-service/src/main/java/cn/com/geasy/marketing/listener/StartupContextListener.java
+++ mkt-service/src/main/java/cn/com/geasy/marketing/listener/StartupContextListener.java
@@ -1,33 +1,30 @@
-///*
-// * Copyright 2016-2018 the original author or authors.
-// * Created on 2018/7/17 下午7:47
-// */
-//package cn.com.geasy.marketing.listener;
-//
-//import cn.com.geasy.marketing.service.shiro.ShiroService;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.ApplicationListener;
-//import org.springframework.context.event.ContextRefreshedEvent;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * 请在此写下该类的说明
-// *
-// * @author phil
-// * @version 1.0.0
-// */
-//@Slf4j
-//@Component
-//public class StartupContextListener implements ApplicationListener<ContextRefreshedEvent> {
-//
-//    private final ShiroService shiroService;
-//
-//    public StartupContextListener(ShiroService shiroService) {
-//        this.shiroService = shiroService;
-//    }
-//
-//    @Override
-//    public void onApplicationEvent(ContextRefreshedEvent event) {
-//        this.shiroService.reloadFilterChains();
-//    }
-//}
+/*
+ * Copyright 2016-2018 the original author or authors.
+ * Created on 2018/7/17 下午7:47
+ */
+package cn.com.geasy.marketing.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * 系统正常启动之后的监听器
+ *
+ * @author phil
+ * @version 1.0.0
+ */
+@Component
+public class StartupContextListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        logger.info("-------------------------------------------");
+        logger.info("Wechat Marketing Platform started.");
+        logger.info("-------------------------------------------");
+    }
+}
