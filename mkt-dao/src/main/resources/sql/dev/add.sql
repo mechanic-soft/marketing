de4f8272cd0a9a9c07f5d3e19236616e0d709ff7
genchengpan <441354512@qq.com>
2018-07-28 22:05:04 +0800

    修改规则行为表的字段的为int

--- mkt-dao/src/main/resources/sql/dev/add.sql
+++ mkt-dao/src/main/resources/sql/dev/add.sql
@@ -3,3 +3,6 @@ ALTER TABLE `wx_contact` MODIFY COLUMN `attr_status`  bigint(11) NULL DEFAULT NU
 
 --聊天记录添加昵称字段
 ALTER TABLE `chat_records` ADD COLUMN `wx_nick_name`  varchar(256) NULL;
+
+--规则行为表的frequency字段的tinyin类型改为INT类型。
+alter table rule_trigger_action MODIFY column frequency INT ;
