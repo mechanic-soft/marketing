d0ad0c585d8797ff9e64ff6b833a533cd3990308
zehongxie <666666>
2018-07-30 17:53:10 +0800

    微信昵称需要转二进制

--- mkt-dao/src/main/resources/sql/dev/add.sql
+++ mkt-dao/src/main/resources/sql/dev/add.sql
@@ -6,3 +6,6 @@ ALTER TABLE `chat_records` ADD COLUMN `wx_nick_name`  varchar(256) NULL;
 
 --规则行为表的frequency字段的tinyin类型改为INT类型。
 alter table rule_trigger_action MODIFY column frequency INT ;
+
+--微信联系人状态字段长度
+ALTER TABLE `wx_contact` MODIFY COLUMN `attr_status`  bigint(20) NULL DEFAULT NULL AFTER `statues`;
