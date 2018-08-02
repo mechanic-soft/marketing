cacf90417147372cb6631d4683539f9fb5e48901
genchengpan <441354512@qq.com>
2018-08-02 10:50:20 +0800

    修改微信好友列表的查询

--- mkt-service/src/main/java/cn/com/geasy/marketing/service/customer/impl/CustomerServiceImpl.java
+++ mkt-service/src/main/java/cn/com/geasy/marketing/service/customer/impl/CustomerServiceImpl.java
@@ -104,7 +104,9 @@ public class CustomerServiceImpl extends SuperServiceImpl<CustomerMapper, Custom
         Long userId = SessionUtils.getUserId();
         EntityWrapper<WxContact> ew=new EntityWrapper<WxContact>();
         ew.where("user_id = {0}",userId);
-        ew.andNew(" nick_name LIKE '%"+nickNameOrRemarkName+"%' " + " OR remark_name LIKE '%"+nickNameOrRemarkName+"%' ");
+        if(StringUtils.isNotBlank(nickNameOrRemarkName)){
+            ew.andNew(" nick_name LIKE '%"+nickNameOrRemarkName+"%' " + " OR remark_name LIKE '%"+nickNameOrRemarkName+"%' ");
+        }
         //将对应实体转换为对应实体的DTO
         List<WxContactDto> corpDtos = WxContactMapstruct.getInstance.toDtoList(wxContactService.selectList(ew));
         return corpDtos;
